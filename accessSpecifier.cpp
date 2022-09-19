606e43f35f52340d2c8cd1d4c84ea490bad4c91a
Sandeep <skumar8work@gmail.com>
2022-09-19 16:21:42 +0530

    Access Specifier in C++👍

---
+++ accessSpecifier.cpp
@@ -0,0 +1,31 @@
+//Access Specifier
+//  Public
+//  Private
+//  Proctected
+#include<iostream>
+using namespace std;
+class PublicClass{
+    
+    public:                 //class member can be accessed by outside of the class
+        string pname = "First Player";
+
+    private:                 //class member can not be accessed and viewd by outside of the class
+        int pnumber = 9809809809;
+
+    protected:                 //class member can be accessed by outside of the class
+        string address = "Bhot dur hai";
+};
+
+int main(){
+    PublicClass Ashok;
+    cout<<"Name of first Player: "<<Ashok.pname;
+    
+    //you'll see following errors with protected 
+    cout<<"Contact Number of the Player: "<<Ashok.pnumber;//"message": "member \"PublicClass::pnumber\" (declared at line 13) is inaccessible",
+		
+	//you'll see following errors with protected 
+    cout<<"Address of the Player"<<Ashok.address; //"message": "member \"PublicClass::address\" (declared at line 16) is inaccessible",
+ 
+    return 0;
+}
+
