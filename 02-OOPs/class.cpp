87372f178f601e46dd1e03f813a58e5ebbdcc58f
Sandeep <skumar8work@gmail.com>
2022-09-19 15:58:10 +0530

    some changes😅

--- 02-OOPs/class.cpp
+++ 02-OOPs/class.cpp
@@ -4,11 +4,22 @@ using namespace std;
 class Car {
         public:
             string name;
+            string brand;
+            string model;
+            int year;
             string color;
+
+            Car(string x,string y,int z,string c){
+                brand = x;
+                model = y;
+                year = z;
+                color = c;
+            }
     };
 
 int main(){
-    Car Tata;
+    Car Tata("Tata","Alto",2022,"Blabla");
+    Car Ford("Fortuner","Desert",2021,"White");
 
     Tata.name= "Nexon";
     Tata.color= "Red";
@@ -16,5 +27,9 @@ int main(){
     cout<<Tata.name;
     cout<<Tata.color;
 
+    cout<<"Car Description:"<<Ford.brand<<"\n";
+    cout<<Ford.model<<"\n";
+    cout<<Ford.year<<"\n";
+    cout<<Ford.color<<"\n";
     return 0;
 }
