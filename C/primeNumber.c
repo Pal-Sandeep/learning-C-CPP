e42b8bad826bfd173d9a4efc3b75da4a71307e09
Sandeep <skumar8work@gmail.com>
2022-09-20 18:13:23 +0530

    Find the Prime Number

---
+++ C/primeNumber.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    int i;
+    int n;  
+    bool flag =true;
+    printf("Enter a number to check its prime or not: ");
+    scanf("%d",&n);
+    for (i=2;i<=n/2;i++){
+        if(n%i==0){
+            printf("%d is Not a prime number;",n);
+            flag = false;
+            break;
+        };
+    }
+    if (flag){
+        printf("%d is a Prime number!",n);
+    }
+
+
+
+    return 0;
+}
