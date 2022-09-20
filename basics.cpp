6ec4e724eb66a6344b21e3b77820889ecf2d8e05
Sandeep <skumar8work@gmail.com>
2022-09-20 10:48:37 +0530

    bascis.cpp added✔

---
+++ basics.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"Please enter your Name: ";
+    cin>>name;
+    cout<<"Welcome to I-Tech Solutions): "<<name<<"\n";
+
+    return 0;
+}
