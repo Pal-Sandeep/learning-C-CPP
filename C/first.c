a71b82ac452747b72382098be3231e4545886907
Sandeep <skumar8work@gmail.com>
2022-09-20 18:09:25 +0530

    first C program

---
+++ C/first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>      //header file library
+
+int main(){             //main functions (everything in main() will be executed)
+    printf("Hii C ! Long time !");
+    
+    return 0;           //return 0 endthe main function
+}
