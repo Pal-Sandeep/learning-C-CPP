fda152c949fa416acf62025590dc911d5b493760
Sandeep <skumar8work@gmail.com>
2022-09-22 07:57:19 +0530

    maximum of array

---
+++ C/maximumofarray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n[10]={1,52,4,21,25,17,8,9,15,24};
+    int bigger=0;
+    int i=0,x;
+    while(i<10){
+        bigger=(n[i]>bigger)?n[i]:bigger;
+        i++;
+    }
+
+    printf("biggest number of arry is : %d",bigger);
+    
+    return 0;
+}
