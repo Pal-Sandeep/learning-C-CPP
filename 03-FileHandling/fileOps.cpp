5c00d1280058bbe72fd59d14730c132b92f10cb0
Sandeep <skumar8work@gmail.com>
2022-09-24 13:26:55 +0530

    still working on it🤞

---
+++ 03-FileHandling/fileOps.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    int line;
+    string text;
+    ofstream countSpaces("demo2.txt");
+    
+    while(countSpaces)
+    cou
+
+    countSpaces.close();
+    return 0;
+}
