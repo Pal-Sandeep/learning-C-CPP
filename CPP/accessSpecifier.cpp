aa28ed2c1140eb8bdc88b2e997bea8b5f772b5d1
Sandeep Pal <skumar8work@gmail.com>
2022-12-22 12:24:09 +0000

    🐛 FIX: minor changes

--- CPP/accessSpecifier.cpp
+++ CPP/accessSpecifier.cpp
@@ -7,13 +7,13 @@ using namespace std;
 // By Default all class member are private 
 class AccessSpecifier{
     
-    public:                 //class member can be accessed by outside of the class
+    public:                             //class member can be accessed by outside of the class
         string pname = "First Player";
 
-    private:                 //class member can not be accessed and viewd by outside of the class
+    private:                            //class member can not be accessed and viewd by outside of the class
         int pnumber = 9809809809;
 
-    protected:                 //class member can be accessed by outside of the class
+    protected:                          //class member can be accessed by outside of the class
         string address = "Bhot dur hai";
 };
 
@@ -22,10 +22,10 @@ int main(){
     cout<<"Name of first Player: "<<Ashok.pname;
 
     //you'll see following errors with protected 
-    cout<<"Contact Number of the Player: "<<Ashok.pnumber;//"message": "member \"PublicClass::pnumber\" (declared at line 13) is inaccessible",
+    cout<<"Contact Number of the Player: "<<Ashok.pnumber;      //"message": "member \"PublicClass::pnumber\" (declared at line 13) is inaccessible",
 		
 	//you'll see following errors with protected 
-    cout<<"Address of the Player"<<Ashok.address; //"message": "member \"PublicClass::address\" (declared at line 16) is inaccessible",
+    cout<<"Address of the Player"<<Ashok.address;               //"message": "member \"PublicClass::address\" (declared at line 16) is inaccessible",
  
     return 0;
 }
